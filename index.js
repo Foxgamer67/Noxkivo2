d509ea91854428dbdcbf7f379e5a42650074e4b9
Foxgamer67 <53092037+Foxgamer67@users.noreply.github.com>
2020-11-19 22:23:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.on ("guildMemberAdd", member => {
+
+	var role = member.guild.roles.find ("name", "✔️ | 𝐕𝐢𝐞𝐰𝐯𝐞𝐫𝐬");
+	member.addRole (role);
+
+})
+
+client.on ("guildMemberRemove", member => {
+
+})
+
 client.once('ready', () => {
 	console.log('Ready!');
 });
